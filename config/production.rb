d6c19befee173bba151baf9f1b09b47222324f3d
Aadarsh Gupta <68748614+infinity1013@users.noreply.github.com>
2021-04-25 00:09:59 +0530

    Update production.rb

--- config/production.rb
+++ config/production.rb
@@ -1,8 +1,4 @@
-#config/production.rb
-start::app.configure do
-     config.assets.compile = true
-     config.cache_classes = true
-     config.serve_static_assets = true
-     config.assets.compile = true
-     config.assets.digest = true
-end
+config.cache_classes = true
+config.serve_static_assets = true
+config.assets.compile = true
+config.assets.digest = true
